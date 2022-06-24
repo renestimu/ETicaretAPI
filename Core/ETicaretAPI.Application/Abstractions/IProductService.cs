b9058aa7cc83a4d6d08c1bb1ce7ca780c8f23eee
renestimu <byumitsiz@gmail.com>
2022-06-24 19:31:35 +0300

    update

--- Core/ETicaretAPI.Application/Abstractions/IProductService.cs
+++ Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETicaretAPI.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace ETicaretAPI.Application.Abstractions
 {
     public interface IProductService
     {
-        
+        List<Product> GetProducts();
     }
 }
---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
